41a726911156df9ce220cc95668d5f8ac10ec34b
peacemakerbak <peacemakerbak@gmail.com>
2024-09-20 18:19:25 -0400

    feat: add new components and styles
    - Added new components for data visualizations
    - Updated styles for better UI

---
+++ README.md
@@ -0,0 +1,36 @@
+# Human Rights First: Asylum Report Generator
+
+## North Star
+- A search tool to give a user-friendly view into a large dataset of asylum case decisions.
+- Similar to the [TRAC](https://trac.syr.edu/phptools/immigration/asylum/) database—a popular existing tool, but with many limitations.
+  - Generate a table of results, a real-time visualization of outcomes, and a real-time graph to represent what the trend looks like over time.
+  - Show a heatmap where users can map out e.g., “Here are the 10 asylum regions with the highest or lowest grant rates.”
+  - Need to be able to download search results.
+
+## Audiences
+- Journalists
+- Researchers
+- General public (asylum seekers, activists, etc.)
+
+## Users
+- Administrators
+  - Engineers who interact directly with infra (don’t need user auth)
+  - Add/manage data
+- Public Users
+  - Don’t need to log in—just view and use the public web app
+  - Search and filter asylum decision data by all columns (as well as any compound combinations)
+  - Display calendar dates vs. fiscal years in data (display “month and year” and “fiscal year” as separate search)
+  - Can view and filter on pre-populated geographic regions based on sets of countries for search parameters
+  - View results in a table and any relevant graphs
+  - View percentage of cases that were granted based on currently displayed results
+  - Should be able to save or print reports, charts, and/or graphs generated from a query
+
+## Resources
+
+**The Setup for this project will require a new LTS Version of NodeJS to be installed Node Version 20.x works well for this purpose.**
+
+The following deployed site shows your exact deliverable and what you should be building: 
+- [Deployed Site](https://asylum-rg-fe.vercel.app/)
+
+This is what the raw data looks like from HRF:
+- [Sample Case Data](https://github.com/BloomTech-Labs/asylum-rg-fe-starter/blob/main/src/data/COW2021001887-I589Data.csv)
---
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+// ADD IMPORTS BACK FOR GRAPHS SECTION
+// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
+import '../../../styles/RenderLandingPage.less';
+import { Button } from 'antd';
+import { useHistory } from 'react-router-dom';
+// for the purposes of testing PageNav
+// import PageNav from '../../common/PageNav';
+
+function RenderLandingPage(props) {
+  const scrollToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
+  const history = useHistory();
+
+  return (
+    <div className="main">
+      <div className="header">
+        <div className="header-text-container">
+          <h1>Asylum Office Grant Rate Tracker</h1>
+          <h3>
+            The Asylum Office Grant Rate Tracker provides asylum seekers,
+            researchers, policymakers, and the public an interactive tool to
+            explore USCIS data on Asylum Office decisions
+          </h3>
+        </div>
+      </div>
+
+      {/* Graphs Section: Add code here for the graphs section for your first ticket */}
+      {/* <div className="graphs-section"> */}
+      <div className="view-more-data-btn-container">
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+        >
+          View the Data
+        </Button>
+      </div>
+
+      <div className="middle-section">
+        <div className="hrf-img-container">
+          <img src={HrfPhoto} alt="Human Rights First" className="hrf-img" />
+        </div>
+        <div className="middle-section-text-container">
+          <h3>
+            Human Rights First has created a search tool to give you a
+            user-friendly way to explore a data set of asylum decisions between
+            FY 2016 and May 2021 by the USCIS Asylum Office, which we received
+            through a Freedom of Information Act request. You can search for
+            information on asylum grant rates by year, nationality, and asylum
+            office, visualize the data with charts and heat maps, and download
+            the data set
+          </h3>
+        </div>
+      </div>
+      <div>
+        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+        {/* <div className="bottom-section">*/}
+        <p onClick={() => scrollToTop()} className="back-to-top">
+          Back To Top ^
+        </p>
+      </div>
+    </div>
+  );
+}
+export default RenderLandingPage;
