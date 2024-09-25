bd6ea186e75d5d1d4e03fd89d2988df571aed357
peacemakerbak <peacemakerbak@gmail.com>
2024-09-25 13:13:15 -0400

    ticket 2 complete

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,7 +10,6 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
@@ -19,12 +18,16 @@ const { background_color } = colors;
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
   let { office, view } = useParams();
+
+  // Set default view if not provided
   if (!view) {
     set_view('time-series');
     view = 'time-series';
   }
-  let map_to_render;
+
+  let map_to_render; // Variable to hold the component to render based on view
   if (!office) {
+    // Render different maps based on the view
     switch (view) {
       case 'time-series':
         map_to_render = <TimeSeriesAll />;
@@ -36,9 +39,11 @@ function GraphWrapper(props) {
         map_to_render = <CitizenshipMapAll />;
         break;
       default:
+        map_to_render = null;
         break;
     }
   } else {
+    // Render maps for a specific office
     switch (view) {
       case 'time-series':
         map_to_render = <TimeSeriesSingleOffice office={office} />;
@@ -47,68 +52,48 @@ function GraphWrapper(props) {
         map_to_render = <CitizenshipMapSingleOffice office={office} />;
         break;
       default:
+        map_to_render = null;
         break;
     }
   }
-  function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
-          _                                                                             _
-        |                                                                                 |
-        |   Example request for once the `/summary` endpoint is up and running:           |
-        |                                                                                 |
-        |     `${url}/summary?to=2022&from=2015&office=ZLA`                               |
-        |                                                                                 |
-        |     so in axios we will say:                                                    |
-        |                                                                                 |     
-        |       axios.get(`${url}/summary`, {                                             |
-        |         params: {                                                               |
-        |           from: <year_start>,                                                   |
-        |           to: <year_end>,                                                       |
-        |           office: <office>,       [ <-- this one is optional! when    ]         |
-        |         },                        [ querying by `all offices` there's ]         |
-        |       })                          [ no `office` param in the query    ]         |
-        |                                                                                 |
-          _                                                                             _
-                                   -- Mack 
-    
-    */
 
-    if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
+  // Function to fetch data from the API and update state
+  async function updateStateWithNewData(years, view, office, stateSettingCallback) {
+    const citizenshipApi = `${process.env.REACT_APP_API_URI}/citizenshipSummary`; // API endpoint for citizenship data
+    const fiscalSummaryAPI = `${process.env.REACT_APP_API_URI}/fiscalSummary`; // API endpoint for fiscal data
+
+    try {
+      let citizenshipData, fiscalData;
+
+      // Fetch data based on office parameter
+      if (office === 'all' || !office) {
+        citizenshipData = await axios.get(citizenshipApi, {
+          params: { from: years[0], to: years[1] },
         });
-    } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
+        fiscalData = await axios.get(fiscalSummaryAPI, {
+          params: { from: years[0], to: years[1] },
         });
+      } else {
+        citizenshipData = await axios.get(citizenshipApi, {
+          params: { from: years[0], to: years[1], office },
+        });
+        fiscalData = await axios.get(fiscalSummaryAPI, {
+          params: { from: years[0], to: years[1], office },
+        });
+      }
+
+      // Combine citizenship data into fiscal data
+      fiscalData.data["citizenshipResults"] = citizenshipData.data;
+      stateSettingCallback(view, office, [fiscalData.data]); // Update state with combined data
+    } catch (error) {
+      console.error('Error fetching data:', error); // Log any errors
     }
   }
+
   const clearQuery = (view, office) => {
-    dispatch(resetVisualizationQuery(view, office));
+    dispatch(resetVisualizationQuery(view, office)); // Reset the visualization query
   };
+
   return (
     <div
       className="map-wrapper-container"
@@ -121,7 +106,7 @@ function GraphWrapper(props) {
       }}
     >
       <ScrollToTopOnMount />
-      {map_to_render}
+      {map_to_render} {/* Render the appropriate map based on the view */}
       <div
         className="user-input-sidebar-container"
         style={{
@@ -132,16 +117,16 @@ function GraphWrapper(props) {
           justifyContent: 'center',
         }}
       >
-        <ViewSelect set_view={set_view} />
+        <ViewSelect set_view={set_view} /> {/* Component to select view */}
         <YearLimitsSelect
           view={view}
           office={office}
           clearQuery={clearQuery}
-          updateStateWithNewData={updateStateWithNewData}
+          updateStateWithNewData={updateStateWithNewData} // Pass the data fetching function
         />
       </div>
     </div>
   );
 }
 
-export default connect()(GraphWrapper);
+export default connect()(GraphWrapper); // Connect the component to Redux
